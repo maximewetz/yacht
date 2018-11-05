7ef2f892a2c7bc5be3508f47c3c64433fe4a5f99
christophe moorkens <christophe.moorkens@gmail.com>
2018-11-05 16:04:28 +0100

    deletemodeluser

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_140806) do
+ActiveRecord::Schema.define(version: 2018_11_05_143057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availibilities", force: :cascade do |t|
+    t.date "available_from"
+    t.date "available_to"
+    t.bigint "boat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_availibilities_on_boat_id"
+  end
+
+  create_table "boats", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.string "city"
+    t.integer "price"
+    t.integer "size"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boats_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "booked_from"
+    t.date "booked_to"
+    t.bigint "user_id"
+    t.bigint "boat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_reservations_on_boat_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "reservation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +73,10 @@ ActiveRecord::Schema.define(version: 2018_11_05_140806) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "availibilities", "boats"
+  add_foreign_key "boats", "users"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "reservations", "boats"
+  add_foreign_key "reservations", "users"
+  add_foreign_key "reviews", "reservations"
 end
