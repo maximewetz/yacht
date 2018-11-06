fa8cd3798ebefa9ae1ecb545fb2de0f0f0d0f047
christophe moorkens <christophe.moorkens@gmail.com>
2018-11-06 12:19:31 +0100

    Addcolumnskippertoreservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -19,30 +19,30 @@ class ReservationsController < ApplicationController
   def create
     @reservation = Reservation.new(reservation_params)
 
-    if @reservation.save
-      redirect_to @reservation, notice: 'Reservation was successfully created.'
-    else
-      render :new
-    end
+    # if @reservation.save
+    #   redirect_to @reservation, notice: 'Reservation was successfully created.'
+    # else
+    #   render :new
+    # end
   end
 
   def update
-    if @reservation.update(reservation_params)
-      redirect_to @reservation, notice: 'Reservation was successfully updated.'
-    else
-      render :edit
-    end
+    # if @reservation.update(reservation_params)
+    #   redirect_to @reservation, notice: 'Reservation was successfully updated.'
+    # else
+    #   render :edit
+    # end
   end
 
   def destroy
     @reservation.destroy
-    redirect_to reservations_url, notice: 'Reservation was successfully destroyed.'
+    # redirect_to reservations_url, notice: 'Reservation was successfully destroyed.'
   end
 
   private
 
     def reservation_params
-      params.require(:reservation).permit(:booked_from, :booked_to)
+      params.require(:reservation).permit(:booked_from, :booked_to, :boat_id, :user_id)
     end
 # end
 end
