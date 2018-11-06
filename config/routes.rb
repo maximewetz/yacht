389ece376611a61263a2118f8c14255a2d5f3408
Julian Stas <julian.stas@gmail.com>
2018-11-06 16:48:53 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -17,18 +17,28 @@ Rails.application.routes.draw do
 
   end
 
+<<<<<<< HEAD
   resources :boats, only: [:show] do
 >>>>>>> 4b4fe70c060ccf52a65f6cc28e7fbfbf85643415
+=======
+    resources :boats, only: [:index, :show] do
+>>>>>>> 1a7df7179d90560357bcc5dc8c01cc38fd6c1b37
     resources :reservations, only: [:new, :create]
     resources :reviews
     resources :availibilities, only: [:index, :show]
   end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   resources :profile do
 =======
   resources :profiles, only: [:show] do
 >>>>>>> 4b4fe70c060ccf52a65f6cc28e7fbfbf85643415
+=======
+
+
+  resources :profiles, only: [:show] do
+>>>>>>> 1a7df7179d90560357bcc5dc8c01cc38fd6c1b37
     resources :reservations, only: [:index, :edit, :update, :show, :destroy]
   end
  end
