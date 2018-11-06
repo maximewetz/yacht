fd4c40c02bd2f816e42ba3f0ff6743e99a5bc3b4
Maxime Wetz <maxime-wetz@hotmail.com>
2018-11-06 12:47:37 +0100

    boatscontroller [working progress]

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,42 @@
+class BoatsController < ApplicationController
+  def index
+    @boats = Boat.all
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :show
+    end
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def update
+    @boat = Boat.find(params[:id])
+    @boat.update(boat_params)
+    redirect_to boat_path(@boat)
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy redirect_to boats_path
+  end
+
+  private
+  def boat_params
+    params.require(:boat).permit(:user_id, :name, :type, :city, :price, :size)
+  end
+end
