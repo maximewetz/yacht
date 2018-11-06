41d49b624686947a911ffcfe23df851c33979460
christophe moorkens <christophe.moorkens@gmail.com>
2018-11-06 12:57:16 +0100

    controllerreservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,24 +9,37 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @boat = Boat.find(params[:boat_id])
+    @user = User.find(params[:user_id])
     @reservation = Reservation.new
   end
 
   def edit
+    @boat = Boat.find(params[:boat_id])
+    @user = User.find(params[:user_id])
     @reservation = Reservation.find(params[:id])
   end
 
   def create
+    @boat = Boat.find(params[:boat_id])
+    @user = User.find(params[:user_id])
     @reservation = Reservation.new(reservation_params)
+    @reservation.boat.user = @reservation
+  #   if current.user == user
 
-    # if @reservation.save
-    #   redirect_to @reservation, notice: 'Reservation was successfully created.'
-    # else
-    #   render :new
-    # end
+  #   # if @reservation.save
+  #   #   redirect_to @reservation, notice: 'Reservation was successfully created.'
+  #   # else
+  #   #   render :new
+  #   # end
+  # end
   end
 
   def update
+    @boat = Boat.find(params[:boat_id])
+    @user = User.find(params[:user_id])
+    @reservation = Reservation.update(reservation_params)
+    @reservation.boat.user = @reservation
     # if @reservation.update(reservation_params)
     #   redirect_to @reservation, notice: 'Reservation was successfully updated.'
     # else
@@ -35,6 +48,9 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
+    @boat = Boat.find(params[:boat_id])
+    @user = User.find(params[:user_id])
+    @reservation.boat.user = @reservation
     @reservation.destroy
     # redirect_to reservations_url, notice: 'Reservation was successfully destroyed.'
   end
