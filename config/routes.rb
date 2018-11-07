4d26a78ef0e15c32bb24a6b7572d5a3d741651f8
christophe moorkens <christophe.moorkens@gmail.com>
2018-11-07 15:17:10 +0100

    reservation

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'boats#index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :reservations, only: [:index, :edit, :update, :destroy, :show]
+  end
   resources :profiles
 
 
   resources :boats do
     resources :reviews
-    resources :availibilities do
-      resources :reservations
-    end
+    resources :availibilities
+    resources :reservations, only: [:create, :new]
   end
  end
