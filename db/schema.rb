c20579d29dec863742274691e964f7816995590a
Julian Stas <julian.stas@gmail.com>
2018-11-08 14:58:35 +0100

    fixed some links

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2018_11_08_130506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "availibilities", force: :cascade do |t|
+    t.date "available_from"
+    t.date "available_to"
+    t.bigint "boat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_availibilities_on_boat_id"
+  end
+
   create_table "boats", force: :cascade do |t|
     t.string "name"
     t.string "boat_type"
@@ -48,8 +57,8 @@ ActiveRecord::Schema.define(version: 2018_11_08_130506) do
     t.index ["boat_id"], name: "index_reservations_on_boat_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
-  
-create_table "reviews", force: :cascade do |t|
+
+  create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "comment"
     t.bigint "boat_id"
@@ -72,11 +81,11 @@ create_table "reviews", force: :cascade do |t|
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "availibilities", "boats"
   add_foreign_key "boats", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "reservations", "boats"
   add_foreign_key "reservations", "users"
   add_foreign_key "reviews", "boats"
   add_foreign_key "reviews", "users"
-
 end
